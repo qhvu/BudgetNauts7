5fac8fbfec968d371c904f0c6dc711b3d6fb6eaa
QH <hqv001@ucsd.edu>
2020-02-20 15:55:29 -0800

    dynamically update budget

--- routes/edit.js
+++ routes/edit.js
@@ -4,14 +4,15 @@ exports.editBudget = function(request, response) {
   var budget = request.query.budget;
   var savings = request.query.savings;
 
+  //edit value to most recent (budget)
+  data.budget[0] = budget;
   
 
-  console.log("budget:" + budget);
-
-  var newEntry = {"budget": budget, "savings": savings};
+  //edit value to most recent (purchase)
+  data.savings[0] = savings;
   
-  data.push(newEntry);
 
-response.render('budget', data);
+  response.render('budget', data);
+
   }
 
